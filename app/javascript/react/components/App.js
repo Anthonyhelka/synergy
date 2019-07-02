c54bb6f5ce597230f623324b11a0ecb7b56d9b4a
Anthonyhelka <helkaanthony1997@gmail.com>
2019-07-02 12:37:18 -0400

    Set up React Router and Semantic UI React. Created HomeContainer.

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import { Router, browserHistory, Route} from 'react-router';
+import 'semantic-ui-css/semantic.min.css';
+
+import HomeContainer from './HomeContainer';
 
 class App extends Component {
-
   render(){
-         return (
-            <div>
-          hello from app.js
-                </div>
-
-            )
-      }
+    return (
+      <div>
+        <Router history={browserHistory}>
+          <Route path='/' component={HomeContainer}/>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
