57034e9417f3c08ca56049b8c09daee1dd57f105
Anthonyhelka <helkaanthony1997@gmail.com>
2019-07-03 12:07:21 -0400

    Adding warning messages for pages still under development.

--- app/javascript/react/components/ItemsContainer.js
+++ app/javascript/react/components/ItemsContainer.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Menu, Container, Header, List, Dropdown } from 'semantic-ui-react';
+import { Menu, Container, Header, List, Dropdown, Message } from 'semantic-ui-react';
 
 class ItemsContainer extends Component {
   render() {
     return (
       <div>
-      <Menu size='large'>
-        <Menu.Item name='home' as={ Link } to='/' >
-          <img src={require('../../../../public/favicon.ico')} />
-        </Menu.Item>
-        <Dropdown item text='Champions'>
-          <Dropdown.Menu>
-            <Dropdown.Item text='Overview' as={ Link } to='/champions/overview' />
-            <Dropdown.Item text='Origin Synergies' as={ Link } to='/champions/origins' />
-            <Dropdown.Item text='Class Synergies' as={ Link } to='/champions/classes' />
-          </Dropdown.Menu>
-        </Dropdown>
-        <Menu.Item name='items' as={ Link } to='/items' active >
-          <p>Items</p>
-        </Menu.Item>
-        <Menu.Item name='news' as={ Link } to='/news' >
-          <p>News</p>
-        </Menu.Item>
-      </Menu>
-        <p>Hello from Items :)</p>
+        <Menu size='large'>
+          <Menu.Item name='home' as={ Link } to='/' >
+            <img src={require('../../../../public/favicon.ico')} />
+          </Menu.Item>
+          <Dropdown item text='Champions'>
+            <Dropdown.Menu>
+              <Dropdown.Item text='Overview' as={ Link } to='/champions/overview' />
+              <Dropdown.Item text='Origin Synergies' as={ Link } to='/champions/origins' />
+              <Dropdown.Item text='Class Synergies' as={ Link } to='/champions/classes' />
+            </Dropdown.Menu>
+          </Dropdown>
+          <Menu.Item name='items' as={ Link } to='/items' active >
+            <p>Items</p>
+          </Menu.Item>
+          <Menu.Item name='news' as={ Link } to='/news' >
+            <p>News</p>
+          </Menu.Item>
+        </Menu>
+
+        <Message warning icon='wrench' header='This Page (Items) is Still Under Development' content='We are sorry for the inconvenience' />
+
       </div>
     )
   }
