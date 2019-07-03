0ca09d3e7b0b626a733368e932a3802db917539e
Anthonyhelka <helkaanthony1997@gmail.com>
2019-07-03 00:47:32 -0400

    Touched up some React in Items and News containers.

--- app/javascript/react/components/NewsContainer.js
+++ app/javascript/react/components/NewsContainer.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Container, Header, List, Dropdown } from 'semantic-ui-react';
 
 class NewsContainer extends Component {
   render() {
     return (
       <div>
-        <Menu size='large'>
-          <Menu.Item name='home' as={ Link } to='/' >
-            <img src={require('../../../../public/favicon.ico')} />
-          </Menu.Item>
-          <Menu.Item name='champions' as={ Link } to='/champions' >
-            <p>Champions</p>
-          </Menu.Item>
-          <Menu.Item name='items' as={ Link } to='/items' >
-            <p>Items</p>
-          </Menu.Item>
-          <Menu.Item name='news' as={ Link } to='/news' active >
-            <p>News</p>
-          </Menu.Item>
-        </Menu>
+      <Menu size='large'>
+        <Menu.Item name='home' as={ Link } to='/' >
+          <img src={require('../../../../public/favicon.ico')} />
+        </Menu.Item>
+        <Dropdown item text='Champions'>
+          <Dropdown.Menu>
+            <Dropdown.Item text='Overview' as={ Link } to='/champions/overview' />
+            <Dropdown.Item text='Origin Synergies' as={ Link } to='/champions/origins' />
+            <Dropdown.Item text='Class Synergies' as={ Link } to='/champions/classes' />
+          </Dropdown.Menu>
+        </Dropdown>
+        <Menu.Item name='items' as={ Link } to='/items' >
+          <p>Items</p>
+        </Menu.Item>
+        <Menu.Item name='news' as={ Link } to='/news' active >
+          <p>News</p>
+        </Menu.Item>
+      </Menu>
         <p>Hello from News :)</p>
       </div>
     )
