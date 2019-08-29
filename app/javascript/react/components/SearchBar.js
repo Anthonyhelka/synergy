fde1d94e419d8cd711bb29c88803dc15f1d4e5e6
Anthonyhelka <helkaanthony1997@gmail.com>
2019-08-29 18:30:38 -0400

    Cleaned up CSS for Home page, navigation bar and search bar.

--- app/javascript/react/components/SearchBar.js
+++ app/javascript/react/components/SearchBar.js
@@ -70,79 +70,39 @@ class SearchBar extends Component {
     const resultRenderer = ({ title, image, id }) => {
       const pathToIcon = require.context('../../../../public/icons', true);
       return [
-        <Responsive key={1} as={Segment} fluid basic='very' textAlign='left' maxWidth={500}  onClick={event => this.handleResultSelect(event, title)}>
-          <img id='search-image-mobile' key={id} src={`${pathToIcon(image , true)}`} />
-          <span id='search-title'>&nbsp;&nbsp;{title}</span>
-        </Responsive>,
-        <Responsive key={2} as={Segment} fluid basic='very' textAlign='left' minWidth={501} maxWidth={1249}  onClick={event => this.handleResultSelect(event, title)}>
-          <img id='search-image-tablet-computer' key={id} src={`${pathToIcon(image , true)}`} />
-          <span id='search-title'>&nbsp;&nbsp;{title}</span>
-        </Responsive>,
-        <Responsive key={3} as={Segment} fluid basic='very' textAlign='left' minWidth={1250}  onClick={event => this.handleResultSelect(event, title)}>
-          <img id='search-image-large-computer' key={id} src={`${pathToIcon(image , true)}`} />
-          <span id='search-title'>&nbsp;&nbsp;{title}</span>
-        </Responsive>
+        <Segment fluid basic='very' textAlign='left' onClick={event => this.handleResultSelect(event, title)}>
+          <img id='search-bar-result-image' key={id} src={`${pathToIcon(image , true)}`} />
+          <span id='search-bar-result-title'>&nbsp;&nbsp;{title}</span>
+        </Segment>
       ];
     };
 
     return (
-      <div>
-        <Responsive maxWidth={500}>
-          <Segment id='search-segment' textAlign='center' basic>
-            <Segment id='search-bar-container' compact basic textAlign='center'>
-              <Search id='search-bar' placeholder='Search for champion/summoner' value={this.state.query} results={results} resultRenderer={resultRenderer} onSearchChange={this.handleSearch} onKeyDown={this.handleSearch} icon={false} showNoResults={false} />
-            </Segment>
-
-            <br />
-
-            <Segment id='search-bar-button-group-mobile' basic textAlign='center'>
-              <Button.Group>
-                <Dropdown as={Button} primary text={this.state.region}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item text='NA' onClick={event => this.handleRegionChange(event, 'NA')} />
-                    <Dropdown.Item text='EUNE' onClick={event => this.handleRegionChange(event, 'EUNE')} />
-                    <Dropdown.Item text='EUW' onClick={event => this.handleRegionChange(event, 'EUW')} />
-                    <Dropdown.Item text='LAN' onClick={event => this.handleRegionChange(event, 'LAN')} />
-                    <Dropdown.Item text='LAS' onClick={event => this.handleRegionChange(event, 'LAS')} />
-                    <Dropdown.Item text='KR' onClick={event => this.handleRegionChange(event, 'KR')} />
-                    <Dropdown.Item text='OCE' onClick={event => this.handleRegionChange(event, 'OCE')} />
-                    <Dropdown.Item text='RU' onClick={event => this.handleRegionChange(event, 'RU')} />
-                    <Dropdown.Item text='BR' onClick={event => this.handleRegionChange(event, 'BR')} />
-                    <Dropdown.Item text='TR' onClick={event => this.handleRegionChange(event, 'TR')} />
-                  </Dropdown.Menu>
-                </Dropdown>
-                <Button icon='search' primary onClick={event => this.handleSearch(event, 'search-button')} />
-              </Button.Group>
-            </Segment>
+      <div className='search-bar'>
+        <Segment id='search-bar-container' textAlign='center' basic>
+          <Segment id='search-bar-region-button-container' compact basic textAlign='center'>
+            <Dropdown id='search-bar-region-button' as={Button} compact primary text={this.state.region}>
+              <Dropdown.Menu>
+                <Dropdown.Item text='NA' onClick={event => this.handleRegionChange(event, 'NA')} />
+                <Dropdown.Item text='EUNE' onClick={event => this.handleRegionChange(event, 'EUNE')} />
+                <Dropdown.Item text='EUW' onClick={event => this.handleRegionChange(event, 'EUW')} />
+                <Dropdown.Item text='LAN' onClick={event => this.handleRegionChange(event, 'LAN')} />
+                <Dropdown.Item text='LAS' onClick={event => this.handleRegionChange(event, 'LAS')} />
+                <Dropdown.Item text='KR' onClick={event => this.handleRegionChange(event, 'KR')} />
+                <Dropdown.Item text='OCE' onClick={event => this.handleRegionChange(event, 'OCE')} />
+                <Dropdown.Item text='RU' onClick={event => this.handleRegionChange(event, 'RU')} />
+                <Dropdown.Item text='BR' onClick={event => this.handleRegionChange(event, 'BR')} />
+                <Dropdown.Item text='TR' onClick={event => this.handleRegionChange(event, 'TR')} />
+              </Dropdown.Menu>
+            </Dropdown>
           </Segment>
-        </Responsive>
-
-        <Responsive minWidth={501}>
-          <Segment id='search-segment' textAlign='center' basic>
-            <Segment id='region-button-container' compact basic textAlign='center'>
-              <Dropdown id='region-button' as={Button} compact primary text={this.state.region}>
-                <Dropdown.Menu>
-                  <Dropdown.Item text='NA' onClick={event => this.handleRegionChange(event, 'NA')} />
-                  <Dropdown.Item text='EUNE' onClick={event => this.handleRegionChange(event, 'EUNE')} />
-                  <Dropdown.Item text='EUW' onClick={event => this.handleRegionChange(event, 'EUW')} />
-                  <Dropdown.Item text='LAN' onClick={event => this.handleRegionChange(event, 'LAN')} />
-                  <Dropdown.Item text='LAS' onClick={event => this.handleRegionChange(event, 'LAS')} />
-                  <Dropdown.Item text='KR' onClick={event => this.handleRegionChange(event, 'KR')} />
-                  <Dropdown.Item text='OCE' onClick={event => this.handleRegionChange(event, 'OCE')} />
-                  <Dropdown.Item text='RU' onClick={event => this.handleRegionChange(event, 'RU')} />
-                  <Dropdown.Item text='BR' onClick={event => this.handleRegionChange(event, 'BR')} />
-                  <Dropdown.Item text='TR' onClick={event => this.handleRegionChange(event, 'TR')} />
-                </Dropdown.Menu>
-              </Dropdown>
-            </Segment>
-            <Segment id='search-bar-container' compact basic textAlign='center'>
-              <Search id='search-bar' placeholder='Search for champion/summoner' value={this.state.query} results={results} resultRenderer={resultRenderer} onSearchChange={this.handleSearch} onKeyDown={this.handleSearch} icon={false} showNoResults={false} />
-            </Segment>
-            <Segment id='search-button-container' compact basic textAlign='center'>
-              <Button id='search-button' icon='search' primary onClick={event => this.handleSearch(event, 'search-button')} />
-            </Segment>
+          <Segment id='search-bar-search-form-container' compact basic textAlign='center'>
+            <Search id='search-bar-search-form' placeholder='Search for champion/summoner' icon={false} showNoResults={false} value={this.state.query} results={results} resultRenderer={resultRenderer} onSearchChange={this.handleSearch} onKeyDown={this.handleSearch}/>
           </Segment>
-        </Responsive>
+          <Segment id='search-bar-go-button-container' compact basic textAlign='center'>
+            <Button id='search-bar-go-button' icon='search' primary onClick={event => this.handleSearch(event, 'search-button')} />
+          </Segment>
+        </Segment>
       </div>
     )
   }
