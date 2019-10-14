fa576dfd86963afbf16b3c7c3208c975def1d640
Anthonyhelka <helkaanthony1997@gmail.com>
2019-10-13 23:06:56 -0400

    Revamping ChampionShowPage.

--- app/javascript/react/components/ChampionShow/ChampionShowTile.js
+++ app/javascript/react/components/ChampionShow/ChampionShowTile.js
@@ -21,112 +21,46 @@ class ChampionShowTile extends Component {
     const pathToAbility = require.context('../../../../../public/abilities', true);
     const pathToRender = require.context('../../../../../public/renders', true);
     const pathToSynergyIcon = require.context('../../../../../public/synergies', true);
-
+    const pathToStylingImages = require.context('../../../../../public/styling-images', true);
 
     let synergiesIconList = this.props.champion.teams.map(synergy => {
-      let name = './test-' + (synergy.name).toLowerCase();
       return (
-        <List.Item id='champion-show-page-synergy-list-item' key={synergy.id}>
-          <div className="synergy-hexagon">
-            <img id='synergy-icon' src={`${pathToSynergyIcon(name, true)}`}  />
-          </div>
-          <span id='champion-show-page-synergy-text'>&nbsp;{synergy.name}</span>
-        </List.Item>
+        <div id='champion-page-champion-header-synergy'>
+          <Image src={`${pathToSynergyIcon(synergy.icon, true)}`}  />
+          <span>&nbsp;{synergy.name}</span>
+        </div>
       )
     });
 
-    // let synergiesChampionList = this.props.champion.teams.map(synergy => {
-    //   let synergyChampions = synergy.champions.filter(champion => {
-    //     if (champion.name === this.props.champion.name) {
-    //       return false;
-    //     } else {
-    //       return true;
-    //     }
-    //   }).map(champion => {
-    //     return (<List.Item key={champion.name} onClick={event => this.handleClick(event, `${champion.name}`)}><Image src={`${pathToIcon(champion.icon, true)}`} size='mini'/></List.Item>)
-    //   });
-    //
-    //   return (
-    //     <Segment textAlign='center' inverted>
-    //       <Header><Image src={`${pathToSynergyIcon(synergy.icon, true)}`}/><br />{synergy.name}</Header>
-    //       <Header.Subheader id='show-page-synergy-description'>{synergy.description}</Header.Subheader>
-    //       <Divider />
-    //       <List horizontal>
-    //         {synergyChampions}
-    //       </List>
-    //     </Segment>
-    //   )
-    // });
-
     return (
       <div>
+      <Segment id='champion-page-container'>
 
-
-        <Segment id='champion-show-page-background' raised>
-        </Segment>
-        <Segment id='champion-show-page-cover' raised>
+        <Segment id='champion-page-search-container'>
         </Segment>
 
-          <Segment id='champion-show-page-container' basic raised>
 
-          <Segment id='champion-show-page-left-column' basic>
-            <Segment id='champion-show-page-header'>
-              <Image id='champion-show-page-header-image' src={`${pathToIcon(this.props.champion.icon, true)}`}  />
-              <div id='champion-show-page-header-title'>
-                <span>&nbsp;Akali</span>
-                <br />
-                <span><i>&nbsp;The Rogue Assassin</i></span>
-              </div>
-            </Segment>
-            <Segment id='champion-show-page-render'>
-              <Image id='champion-show-page-render-image' src={`${pathToRender(this.props.champion.icon, true)}`}  />
-            </Segment>
+
+        <Segment id='champion-page-champion-container'>
+          <Segment id='champion-page-champion-header'>
+            <div id='champion-page-champion-header-title'>
+              <span>&nbsp;Akali</span>
+              <br />
+              <i>&nbsp;The Rogue Assassin</i>
+            </div>
+            <Image id='champion-page-champion-header-image' src={`https://cdn.lolchess.gg/images/lol/champion-splash-modified/${this.props.champion.name}.jpg`} />
+            <div id='champion-page-champion-header-synergies-container'>
+              {synergiesIconList}
+            </div>
           </Segment>
-
-          <Segment id='champion-show-page-right-column' basic>
-            <Segment id='champion-show-page-synergy-container'>
-              <List horizontal id='champion-show-page-synergy-list'>{synergiesIconList}</List>
-            </Segment>
-
-          </Segment>
-
-          <Segment id='champion-show-page-bottom-row'>
-          </Segment>
-
         </Segment>
 
+
+
+      </Segment>
       </div>
     );
   }
 }
 
 export default ChampionShowTile;
-
-//   <Responsive maxWidth={1023}>
-//     <Segment>
-//       <Segment textAlign='center' fluid>
-//         <Header><Image src={`${pathToIcon(this.props.champion.icon, true)}`}/><br />{this.props.champion.name}</Header>
-//         <List horizontal>{synergiesIconList}</List>
-//       </Segment>
-//       <Segment textAlign='center' fluid>
-//         <Header><Image id='show-page-ability-icon' src={`${pathToAbility(this.props.champion.icon, true)}`}/><br />{this.props.champion.ability_name}</Header>
-//         <Header.Subheader id='show-page-ability-description'>{this.props.champion.ability_description}</Header.Subheader>
-//         <i>{this.props.champion.mana_cost} Mana</i>
-//       </Segment>
-//       <Header textAlign='center'>Statistics</Header>
-//       <Segment id='champion-show-container' fluid>
-//         <Grid centered>
-//           <Grid.Row><b>Cost:</b>&nbsp;&nbsp;{this.props.champion.tier}</Grid.Row>
-//           <Grid.Row><b>Health:</b>&nbsp;&nbsp;{this.props.champion.health[0]}/{this.props.champion.health[1]}/{this.props.champion.health[2]}</Grid.Row>
-//           <Grid.Row><b>Attack Damage:</b>&nbsp;&nbsp;{this.props.champion.attack_damage}</Grid.Row>
-//           <Grid.Row><b>Attack Speed:</b>&nbsp;&nbsp;{this.props.champion.attack_speed}</Grid.Row>
-//           <Grid.Row><b>Range:</b>&nbsp;&nbsp;{this.props.champion.attack_range}</Grid.Row>
-//           <Grid.Row><b>Armor:</b>&nbsp;&nbsp;{this.props.champion.armor}</Grid.Row>
-//           <Grid.Row><b>Magic Resist:</b>&nbsp;&nbsp;{this.props.champion.magic_resist}</Grid.Row>
-//           <Grid.Row><b>Player Damage:</b>&nbsp;&nbsp;{this.props.champion.player_damage[0]}/{this.props.champion.player_damage[1]}/{this.props.champion.player_damage[2]}</Grid.Row>
-//         </Grid>
-//       </Segment>
-//       <Header textAlign='center'>Synergies</Header>
-//       {synergiesChampionList}
-//     </Segment>
-//   </Responsive>
