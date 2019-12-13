de6f0d4ea01e73c886018140355d4f9c65494230
Anthonyhelka <helkaanthony1997@gmail.com>
2019-12-12 19:43:19 -0500

    Started working towards fixing RSpec tests.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,37 +1,47 @@
 require "factory_bot_rails"
 
-ability_type = ["Active", "Passive"]
-tier = [1, 2, 3, 4, 5]
-tier_color = ["white", "green", "blue", "purple", "yellow"]
-origin = ["Demon", "Dragon", "Exile", "Glacial", "Hextech", "Imperial", "Ninja", "Noble", "Phantom", "Pirate", "Robot", "Void", "Wild", "Yordle"]
-character_class = ["Assassin", "Blademaster", "Brawler", "Elementalist", "Guardian", "Gunslinger", "Knight", "Ranger", "Shapeshifter", "Sorcerer"]
-def random_number
-  return rand 1..100
-end
-name = ["Demon", "Dragon", "Exile", "Glacial", "Hextech", "Imperial", "Ninja", "Noble", "Phantom", "Pirate", "Robot", "Void", "Wild", "Yordle", "Assassin", "Blademaster", "Brawler", "Elementalist", "Guardian", "Gunslinger", "Knight", "Ranger", "Shapeshifter", "Sorcerer"]
-team_type = ["Origin", "Class"]
-
 FactoryBot.define do
+  factory :season do
+    id  { 1 }
+    name { "1" }
+  end
+
   factory :champion do
-    name { Faker::Superhero.name }
-    icon { "./aatrox" }
-    ability_name { Faker::Superhero.power }
-    ability_icon { "./aatrox" }
-    ability_type { ability_type.sample }
-    ability_description { Faker::Hipster.sentences(number: 1) }
-    tier { tier.sample }
-    tier_color { tier_color.sample }
-    origin_1 { origin.sample }
-    character_class_1 { character_class.sample }
-    health { [random_number, random_number, random_number] }
-    starting_mana { random_number }
-    mana_cost { random_number }
-    attack_damage { random_number }
-    attack_speed { rand 1.0..100.0 }
-    attack_range { random_number }
-    armor { random_number }
-    magic_resist { random_number }
-    player_damage { [random_number, random_number, random_number] }
+    association :season
+    key { "aatrox" }
+    name { "Aatrox" }
+    title { "The Darkin Blade" }
+    origins { ["Demon"] }
+    classes { ["Blademaster"] }
+    cost { 3 }
+    ability { {
+      name: "The Darkin Blade",
+      description: "Aatrox cleaves the area in front of him, dealing damage to enemies inside it.",
+      type: "Active",
+      mana_cost: 75,
+      mana_start: 0,
+      stats: [
+        {
+          type: "Damage",
+          value: "300 / 600 / 900"
+        }
+      ]
+    } }
+    stats { {
+      offense: {
+        damage_scale_factor: 1.80,
+        attack_damage: 65,
+        attack_speed: 0.65,
+        range: 1
+      },
+      defense: {
+        health_scale_factor: 1.80,
+        health: 700,
+        armor: 35,
+        magic_resist: 20
+      }
+    } }
+    items { ["WIP"] }
   end
 
   factory :team do
